a42b2dc1aa664f98a8b91cfd992ce188c6893c94
Alexandr <sten.marsh83@gmail.com>
2023-07-11 17:35:58 +0300

    more human friendly player

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ from typing import Optional
 from client import SumerianBot
 import discord
 import os
+import math
 
 class Player(discord.ui.View):
     client = None
@@ -15,52 +16,73 @@ class Player(discord.ui.View):
         
         self.add_sound_selector()
         
+    ##############################################################################
+    ####################    CONTROL BUTTONS     ##################################
+    ##############################################################################
         
     @discord.ui.button(label="Resume", style=discord.ButtonStyle.green, row=3)
     async def resume(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.client.resume_sound()
-        await interaction.response.edit_message(content=f"# Player \n {self.client.playlist[0]}")
+        await interaction.response.edit_message(content=f"# Player")
         
     
     @discord.ui.button(label="Pause", style=discord.ButtonStyle.primary, row=3)
     async def pause(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.client.pause_sound()
-        await interaction.response.edit_message(content=f"# Player ||\n {self.client.playlist[0]}")
+        await interaction.response.edit_message(content=f"# Player ||")
         
     
     @discord.ui.button(label="Skip", style=discord.ButtonStyle.secondary, row=3)
     async def skip(self, interaction: discord.Interaction, button: discord.ui.Button):
         embed = self.client.skip_sound()
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.edit_message(embed=self.client.show_playlist())
+        await interaction.followup.send(embed=embed)
     
         
     @discord.ui.button(label="Stop", style=discord.ButtonStyle.red, row=3)
     async def stop(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.client.stop_playing()
-        await interaction.response.edit_message(content=f"# Player")
+        await interaction.response.edit_message(content=f"# Player", embed=self.client.show_playlist())
         
         
     @discord.ui.button(label="Leave", style=discord.ButtonStyle.danger, row=3)
     async def leave(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.client.disconnect()
-        await interaction.response.edit_message(content=f"# Player")
+        await interaction.response.edit_message(content=f"# Player", embed=None)
+        pass
         
         
-    def soundlist_selection(self, list_index) -> list:
+    ##############################################################################
+    ####################    SOUND SELECTOR     ###################################
+    ##############################################################################    
+        
+        
+    def get_sound_variants(self) -> tuple:
         soundlist = os.listdir(self.client.sound_dir[:-1])
-        selections = []
-        result = []
-        for sound in soundlist:
-            selections.append(discord.SelectOption(label=sound))
+        
+        raw_result = []
+        
+        for i in range(0, len(soundlist), 25):
+            raw = soundlist[i: i + 25]
+            raw_result.append(raw)
+        
+        prepared_result = []
+        
+        for page in raw_result:
+            prepared_page = []
+            for sound in page:
+                prepared_page.append(discord.SelectOption(label=sound))
+                
+            prepared_result.append(prepared_page)
             
-        for i in range(0, len(selections), 25):
-            selection = selections[i: i + 25]
-            result.append(selection)
-            
-        return (result[list_index], len(soundlist))
+        return (raw_result, prepared_result, len(soundlist))
+        pass
+    
     
     def add_sound_selector(self):
-        selector = discord.ui.Select(placeholder=f"Select sound (page: {self.sound_page})", options=self.soundlist_selection(self.sound_page)[0], row=0)
+        sound_variants = self.get_sound_variants()
+        
+        selector = discord.ui.Select(placeholder=f"Select sound (page: {self.sound_page + 1})", options=sound_variants[1][self.sound_page], row=0)
         
         async def select_sound(interaction: discord.Interaction):
             if self.client.voice == None:
@@ -68,7 +90,7 @@ class Player(discord.ui.View):
                 await self.client.connectToVoice(voice)
                 
             embed = self.client.add_playlist(selector.values[0])
-            await interaction.response.edit_message(content=f"# Player \n {self.client.playlist[0]}")
+            await interaction.response.edit_message(content=f"# Player", embed=self.client.show_playlist())
             await interaction.followup.send(embed=embed)
             await self.client.start_sound()
         
@@ -76,9 +98,13 @@ class Player(discord.ui.View):
         
         self.add_item(selector)
         
+        
         pages = []
-        for i in range(round(self.soundlist_selection(0)[1] / 25)):
-            pages.append(discord.SelectOption(label=i))
+        
+        for i in range(math.ceil(sound_variants[2] / 25)):
+            # [0][i][0][0] - raw_result -> i page -> first list item -> first test symbol
+            # [0][i][-1][0] - raw_result -> i page -> last list item -> first test symbol
+            pages.append(discord.SelectOption(label=f"Page {i + 1}: {sound_variants[0][i][0][0]} - {sound_variants[0][i][-1][0]}", value=i))
         
         page = discord.ui.Select(placeholder="Pages", options=pages, row=1)
         
@@ -89,4 +115,5 @@ class Player(discord.ui.View):
         page.callback = select_page
         
         self.add_item(page)
+        pass
             
